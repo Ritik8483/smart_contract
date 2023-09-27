cb12505c398e0cbfe815e114f0f9fdf7224b378a
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2023-09-27 11:16:41 +0530

    add comments

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -7,6 +7,7 @@ contract Transactions {             //defines main contract called Transactions
 
     //address = type, from=variable name
 // Events are used to log important information on the blockchain. In this case, the Transfer event is defined with various parameters that represent details of a transaction.
+// it emits an event Transfer whenever a new transaction is added to the blockchain
      event Transfer(     
         address from,
         address receiver,
@@ -15,7 +16,7 @@ contract Transactions {             //defines main contract called Transactions
         uint256 timestamp,
         string keyword
     );
-// It specifies the properties that a transaction will have, including sender
+// It specifies the properties that a transaction will have, including sender.This is a custom data structure that represents a transaction
     struct TransferStruct {         
         address sender; //what properties this oject have and of what type they are going to be
         address receiver;
@@ -25,18 +26,18 @@ contract Transactions {             //defines main contract called Transactions
         string keyword;
     }
 
-    TransferStruct[] transactions; //means transactions variable is array of objects
+    TransferStruct[] transactions; //means transactions variable is array of objects.It is an array of transfetStruct object
 
     //since this is a class we have to define a visibility function
 
     function addToBlockchain(
-        address payable receiver,
-        uint amount,
-        string memory message,
-        string memory keyword
+        address payable receiver,   //The Ethereum address of the receiver of the transaction.(Metamask account address)
+        uint amount,        //The amount of cryptocurrency to be transferred in the transaction.(amount like 0.00001)
+        string memory message,      //A string containing a message associated with the transaction.(Hello transaction)
+        string memory keyword       //Another string that acts as a keyword associated with the transaction.(Hello keyword)
     ) public {
-        transactionCount += 1;
-
+        transactionCount += 1;      //This line increments a counter named transactionCount by 1, which likely keeps track of the total number of transactions.
+//creates a new TransferStruct object with the provided details, pushes it into the transactions array, and emits a Transfer event to log the transaction
         transactions.push(
             TransferStruct(
                 msg.sender,
@@ -49,7 +50,7 @@ contract Transactions {             //defines main contract called Transactions
         );
 
         emit Transfer(          //emits the Transfer event to log the transaction details. which represents details of transaction
-            msg.sender,
+            msg.sender,             //emitting events is a way to log information about transactions on the blockchain. 
             receiver,
             amount,
             message,
@@ -58,8 +59,9 @@ contract Transactions {             //defines main contract called Transactions
         );
     }
 
-    function getAllTransactions()
-        public
+// is public (can be called by anyone), has the view modifier (it does not modify the state of the contract), and returns an array of TransferStruct objects.
+    function getAllTransactions()  
+        public                  
         view
         returns (TransferStruct[] memory)
     {
@@ -70,3 +72,7 @@ contract Transactions {             //defines main contract called Transactions
         return transactionCount;        //returns the current value of transactionCount, indicating the total number of transactions recorded on the blockchain using this contract.
     }
 }
+
+
+//In this code snippet, emit is used to trigger the Transfer event. The event is then logged on the Ethereum blockchain, 
+//and its data can be accessed by external applications, including React front-ends, by listening to events emitted by the contract.
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {    //The object contains properties that define how Hardhat b
   solidity: '0.8.1',
   networks: {
     sepolia: {
-      url: 'https://eth-sepolia.g.alchemy.com/v2/yNF6jmjoL349mj_CmznpaseJMuvG_i1Z',
+      url: 'https://eth-sepolia.g.alchemy.com/v2/yNF6jmjoL349mj_CmznpaseJMuvG_i1Z',     //Alchemy is a service that provides Ethereum nodes and infrastructure.
       accounts: ['429e5438078f99c07fefb922b7c9b207b684f157f9e9c28f159dc210f58ec8f0'],
     },
   },
